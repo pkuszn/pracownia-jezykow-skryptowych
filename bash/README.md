71201801862f77ffcf6f138ad34fd29d1d9cd2d3
pkusz-wsl <patryk.kuszneruk1798@gmail.com>
2023-11-25 21:00:29 +0100

    Fixed computer_turn(). Added check winner functions

---
+++ bash/README.md
@@ -0,0 +1,8 @@
+# Usage
+```bash
+bash main.sh
+```
+
+# Requirements
+ - GNU bash, version 5.0.17(1)-release (x86_64-pc-linux-gnu)
+ - WSL / Linux
--- bash/src/main.sh
+++ bash/src/main.sh
@@ -3,6 +3,7 @@
 declare -A matrix
 rows=3
 cols=3
+game_over=false
 
 function init() {
     for ((i=1;i<=rows;i++)) do
@@ -44,8 +45,11 @@ function game() {
         "1")
             echo "you chose $start_opt"
             init
-            next_turn
-            print
+            while [ "$game_over" = false ]; do
+                user_turn
+                sleep 1
+                computer_turn
+            done
             ;;
         "2")
             echo "you chose $load_opt"
@@ -56,29 +60,49 @@ function game() {
     esac
 }
 
-function next_turn() {
-    readonly next_turn_msg="Next turn..."
+function user_turn() {
+    user_turn_msg="Now is user turn..."
+    chose_cell_msg="Select cell... ([1-3][1-3])"
+    again_msg="Wrong cell. Select cell again... ([1-3][1-3])"
+    row_msg="Row: "
+    col_msg="Col: "
+    local empty_field=false
+    local empty_field_sign="?"
+
+    print_new_line
     print
-    readonly chose_cell_msg="Chose cell.. ([1-3][1-3])"
-    readonly row_msg="Row..."
-    readonly col_msg="Col..."
-    
-    read -p "${row_msg}" row
-    read -p "${col_msg}" col
+    print_new_line
+    echo "$user_turn_msg"
+    print_new_line
+    echo "$chose_cell_msg"
 
-    matrix[$row,$col]="X"
+    while [ "$empty_field" = false ]; do
+        read -r -p "${col_msg}" col    
+        read -r -p "${row_msg}" row
+        if [ "${matrix[$col,$row]}" = "$empty_field_sign" ]; then
+            matrix[$col,$row]="X"
+            empty_field=true
+        else
+            echo "$again_msg"
+            print_new_line
+            print
+        fi
+    done
 
-    computer_turn
+    print_new_line
+    check_for_winner "X"
 }
 
 function computer_turn() {
-    readonly computer_turn_msg="Now is computer turn..."
+    computer_turn_msg="Now is computer turn..."
     declare -A arrays_of_index
-    local target="?"
+    local empty_field_sign="?"
     local inc=0
-    for ((i=1; i<=$rows; i++)); do
-        for ((j=1; j<=$cols; j++)); do
-            if [ "${matrix[$i,$j]}" = "$target" ]; then
+
+    print
+    for ((i=1; i<=cols; i++)); do
+        for ((j=1; j<=rows; j++)); do
+            if [ "${matrix[$i,$j]}" = "$empty_field_sign" ]; then
                 indexes=("${i}, ${j}")
                 arrays_of_index[$inc]=$indexes
                 ((inc++))
@@ -90,28 +114,37 @@ function computer_turn() {
         return
     fi
 
+    print_new_line
     for index in "${!arrays_of_index[@]}"; do
         echo "Index $index: ${arrays_of_index[$index]}"
     done
 
-    printf "$computer_turn_msg"
+    print_new_line
+    echo "$computer_turn_msg"
+    print_new_line
+
     random_index=$((RANDOM % ${#arrays_of_index[@]}))
+
+    echo "Computer selected index is <$random_index>"
+
     random_value="${arrays_of_index[$random_index]}"
-    IFS=',' read -r r c <<< "$random_value"
+    IFS=',' read -r c r <<< "$random_value"
 
-    row=$((r+0))
     column=$((c+0))
+    row=$((r+0))
 
-    matrix["$row","$column"]="O"
-    printf "\n"
+    echo "Computer selected position is [$column][$row]"
+    print_new_line
 
-    save_to_file "./data/file.txt"
+    matrix["$column","$row"]="O"
+
+    check_for_winner "O"
 }
 
 function save_to_file() {
-    filename=$1
-    for ((r = 1; r <= 3; r++)); do
-        for ((c = 1; c <= 3; c++)); do
+    local filename=$1
+    for ((r = 1; r <= cols; r++)); do
+        for ((c = 1; c <= rows; c++)); do
             echo -n "${matrix[$c,$r]} " >> "$filename"
         done
         echo >> "$filename"
@@ -119,8 +152,8 @@ function save_to_file() {
 }
 
 function read_from_file() {
-    filename=$1
-    r=1
+    local filename=$1
+    local r=1
     while IFS= read -r line; do
         line=$(echo "$line" | tr -d '[:space:]')
         c=1
@@ -133,6 +166,83 @@ function read_from_file() {
     done < "$filename"
 }
 
+function check_for_winner() {
+    local player=$1
+
+    echo "Checking if $player won..."
+
+    check_rows_or_columns "$player"
+    check_diagonals "$player"
+}
+
+function check_rows_or_columns() {
+    local player=$1
+
+    for ((i = 1; i < 3; i++)); do
+        if [[ ${matrix[$i,1]} == "$player" && ${matrix[$i,2]} == "$player" && ${matrix[$i,3]} == "$player" ]] || \
+           [[ ${matrix[1,$i]} == "$player" && ${matrix[2,$i]} == "$player" && ${matrix[3,$i]} == "$player" ]]; then
+            echo "Player $player wins!"
+            exit 0
+        fi
+    done
+}
+
+function check_diagonals() {
+    local player=$1
+
+    if [[ ${matrix[1,1]} == "$player" && ${matrix[2,2]} == "$player" && ${matrix[3,3]} == "$player" ]] || \
+       [[ ${matrix[1,3]} == "$player" && ${matrix[2,2]} == "$player" && ${matrix[3,1]} == "$player" ]]; then
+        echo "Player $player wins!"
+        exit 0
+    fi
+}
+
+function print_new_line() {
+    printf "\n"
+}
+
+# Main function
 game
-# read_from_file "./data/file.txt"
-# print
+
+
+# Cases
+
+# C1
+# |X| | |
+# |X| | |
+# |X| | |
+
+# C2
+# | |X| |
+# | |X| |
+# | |X| |
+
+# C3
+# | | |X|
+# | | |X|
+# | | |X|
+
+# C4
+# |X|X|X|
+# | | | |
+# | | | |
+
+# C5
+# | | | |
+# |X|X|X|
+# | | | |
+
+# C6
+# | | | |
+# | | | |
+# |X|X|X|
+
+# C7
+# |X| | |
+# | |X| |
+# | | |X|
+
+# C8
+# | | |X|
+# | |X| |
+# |X| | |
