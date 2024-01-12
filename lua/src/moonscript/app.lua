2d3a37d7e8c009266b76655b4389ea43434806ad
pkuszn <patryk.kuszneruk1798@gmail.com>
2024-01-12 18:11:09 +0100

    api works

--- lua/src/moonscript/app.lua
+++ lua/src/moonscript/app.lua
@@ -1,10 +1,13 @@
 local lapis = require("lapis")
-local Model = require("lapis.db.model").Model
+local Model
+Model = require("lapis.db.model").Model
 local csrf = require("lapis.csrf")
 local capture_errors = require("lapis.application").capture_errors
 local respond_to
 respond_to = require("lapis.application").respond_to
-local category
+local json_params
+json_params = require("lapis.application").json_params
+local Category
 do
   local _class_0
   local _parent_0 = Model
@@ -16,7 +19,7 @@ do
       return _class_0.__parent.__init(self, ...)
     end,
     __base = _base_0,
-    __name = "category",
+    __name = "Category",
     __parent = _parent_0
   }, {
     __index = function(cls, name)
@@ -40,7 +43,197 @@ do
   if _parent_0.__inherited then
     _parent_0.__inherited(_parent_0, _class_0)
   end
-  category = _class_0
+  Category = _class_0
+end
+local PaymentType
+do
+  local _class_0
+  local _parent_0 = Model
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "PaymentType",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  PaymentType = _class_0
+end
+local DeliveryType
+do
+  local _class_0
+  local _parent_0 = Model
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "DeliveryType",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  DeliveryType = _class_0
+end
+local Product
+do
+  local _class_0
+  local _parent_0 = Model
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "Product",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  Product = _class_0
+end
+local Purchase
+do
+  local _class_0
+  local _parent_0 = Model
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "Purchase",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  Purchase = _class_0
+end
+local User
+do
+  local _class_0
+  local _parent_0 = Model
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _class_0.__parent.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "User",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  User = _class_0
 end
 local App
 do
@@ -54,42 +247,302 @@ do
         }
       end
     }),
-    ["/get_category"] = respond_to({
-      before = function(self)
-        do
-          local _base_1 = category
-          local _fn_0 = _base_1.select
-          self.user = function(...)
-            return _fn_0(_base_1, ...)
-          end
-        end
+    ["/get-categories"] = respond_to({
+      GET = function(self)
+        self.user = Category:select()
         if not (self.user) then
-          return self:write({
+          self:write({
             status = 404
           }, "Not Found")
         end
-      end,
-      GET = function(self)
         return {
           json = self.user
         }
       end
-    }, {
-      ["/get_category/:id"] = respond_to({
-        before = function(self)
-          self.user = category:find(self.params.id)
-          if not (self.user) then
-            return self:write({
-              status = 404
-            }, "Not Found")
-          end
-        end,
-        GET = function(self)
-          return {
-            json = self.user
-          }
+    }),
+    ["/get-category/:id"] = respond_to({
+      GET = function(self)
+        self.user = Category:find({
+          id = self.params.id
+        })
+        if not (self.user) then
+          self:write({
+            status = 404
+          }, "Not Found")
         end
-      })
+        return {
+          json = self.user
+        }
+      end
+    }),
+    ["/get-payment-types"] = respond_to({
+      GET = function(self)
+        self.payment_type = PaymentType:select()
+        if not (self.payment_type) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.payment_type
+        }
+      end
+    }),
+    ["/get-payment-type/:id"] = respond_to({
+      GET = function(self)
+        self.payment_type = PaymentType:find({
+          id = self.params.id
+        })
+        if not (self.payment_type) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.payment_type
+        }
+      end
+    }),
+    ["/get-delivery-types"] = respond_to({
+      GET = function(self)
+        self.delivery_type = DeliveryType:select()
+        if not (self.delivery_type) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.delivery_type
+        }
+      end
+    }),
+    ["/get-delivery-type/:id"] = respond_to({
+      GET = function(self)
+        self.delivery_type = DeliveryType:find({
+          id = self.params.id
+        })
+        if not (self.delivery_type) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.delivery_type
+        }
+      end
+    }),
+    ["/products"] = respond_to({
+      GET = function(self)
+        self.product = Product:select()
+        if not (self.product) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.product
+        }
+      end,
+      POST = json_params(function(self)
+        self.product = Product:create({
+          name = self.params.name,
+          category = tonumber(self.params.category),
+          price = tonumber(self.params.price),
+          created_date = os.date("%Y-%m-%d"),
+          description = self.params.description,
+          available = not not self.params.available
+        })
+        return {
+          json = self.product
+        }
+      end)
+    }),
+    ["/product"] = respond_to({
+      PUT = json_params(function(self)
+        self.product = Product:find({
+          id = self.params.id
+        })
+        if not (self.product) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.product:update(self.params)
+        return {
+          json = self.product
+        }
+      end)
+    }),
+    ["/product/:id"] = respond_to({
+      GET = function(self)
+        self.product = Product:find({
+          id = self.params.id
+        })
+        if not (self.product) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.product
+        }
+      end,
+      DELETE = function(self)
+        self.product = Product:find({
+          id = self.params.id
+        })
+        if not (self.product) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.product:delete()
+        return {
+          json = "Item deleted"
+        }
+      end
+    }),
+    ["/purchases"] = respond_to({
+      GET = function(self)
+        self.purchase = Purchase:select()
+        if not (self.purchase) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.purchase
+        }
+      end,
+      POST = json_params(function(self)
+        self.purchase = Purchase:create({
+          id_product = tonumber(self.params.id_product),
+          id_user = tonumber(self.params.id_user),
+          price = tonumber(self.params.price),
+          quantity = tonumber(self.params.quantity),
+          purchase_date = os.date("%Y-%m-%d"),
+          delivery_type = tonumber(self.params.delivery_type),
+          payment_type = tonumber(self.params.payment_type)
+        })
+        return {
+          json = self.purchase
+        }
+      end)
+    }),
+    ["/purchase"] = respond_to({
+      PUT = json_params(function(self)
+        self.purchase = Purchase:find({
+          id = self.params.id
+        })
+        if not (self.purchase) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.purchase:update(self.params)
+        return {
+          json = self.purchase
+        }
+      end)
+    }),
+    ["/purchase/:id"] = respond_to({
+      GET = function(self)
+        self.purchase = Purchase:find({
+          id = self.params.id
+        })
+        if not (self.purchase) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.purchase
+        }
+      end,
+      DELETE = function(self)
+        self.purchase = Purchase:find({
+          id = self.params.id
+        })
+        if not (self.purchase) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.purchase:delete()
+        return {
+          json = "Item deleted"
+        }
+      end
+    }),
+    ["/users"] = respond_to({
+      GET = function(self)
+        self.user = User:select()
+        if not (self.user) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.user
+        }
+      end,
+      POST = json_params(function(self)
+        self.user = User:create({
+          name = self.params.name,
+          surname = self.params.surname,
+          password = self.params.password,
+          address = self.params.address,
+          created_date = os.date("%Y-%m-%d")
+        })
+        return {
+          json = self.user
+        }
+      end)
+    }),
+    ["/user"] = respond_to({
+      PUT = function(self)
+        self.user = User:find({
+          id = self.params.id
+        })
+        if not (self.user) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.user:update(self.params)
+        return {
+          json = self.user
+        }
+      end
+    }),
+    ["/user/:id"] = respond_to({
+      GET = function(self)
+        self.user = User:find({
+          id = self.params.id
+        })
+        if not (self.user) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        return {
+          json = self.user
+        }
+      end,
+      DELETE = function(self)
+        self.user = User:find({
+          id = self.params.id
+        })
+        if not (self.user) then
+          self:write({
+            status = 404
+          }, "Not Found")
+        end
+        self.user:delete()
+        return {
+          json = "Item deleted"
+        }
+      end
     })
   }
   _base_0.__index = _base_0
--- lua/src/moonscript/app.moon
+++ lua/src/moonscript/app.moon
@@ -1,229 +1,172 @@
 lapis = require("lapis")
-Model = require("lapis.db.model").Model
+import Model from require "lapis.db.model"
 csrf = require "lapis.csrf"
 capture_errors = require("lapis.application").capture_errors
 import respond_to from require "lapis.application"
+import json_params from require "lapis.application"
 
-class category extends Model
-    -- purchase = Model.extend 'purchase'
--- product = Model.extend 'product'
--- delivery_type = Model.extend 'delivery_type'
--- payment_type = Model.extend 'payment_type'
--- user = Model.extend 'user'
+
+class Category extends Model
+class PaymentType extends Model
+class DeliveryType extends Model
+class Product extends Model
+class Purchase extends Model
+class User extends Model
 
 class App extends lapis.Application
     "/hello": respond_to {
         GET: =>
             { json: "hello world"}
-      }
+    }
 
-    "/get_category": respond_to {
-        before: =>
-            @user = category\select
-            @write status: 404, "Not Found" unless @user
-    
+    "/get-categories": respond_to {
         GET: =>
+            @user = Category\select!
+            @write status: 404, "Not Found" unless @user
             {json: @user }
-      }
+    }
 
-      "/get_category/:id": respond_to {
-        before: =>
-            @user = category\find @params.id
-            @write status: 404, "Not Found" unless @user
-    
+    "/get-category/:id": respond_to {    
         GET: =>
-          { json: @user }
-      }
+            @user = Category\find id: @params.id
+            @write status: 404, "Not Found" unless @user
+            { json: @user }
+    }
 
+    "/get-payment-types": respond_to {
+        GET: =>
+            @payment_type = PaymentType\select!
+            @write status: 404, "Not Found" unless @payment_type
+            {json: @payment_type }
+    }
 
+    "/get-payment-type/:id": respond_to {
+        GET: => 
+            @payment_type = PaymentType\find id: @params.id
+            @write status: 404, "Not Found" unless @payment_type
+            { json: @payment_type }
+    }
 
--- app:get "hello", "/hello", ->
---   "Welcome to profile"
+    "/get-delivery-types": respond_to {
+        GET: => 
+            @delivery_type = DeliveryType\select!
+            @write status: 404, "Not Found" unless @delivery_type
+            { json: @delivery_type }
+    }
 
--- -- Category
+    "/get-delivery-type/:id": respond_to {
+        GET: => 
+            @delivery_type = DeliveryType\find id: @params.id
+            @write status: 404, "Not Found" unless @delivery_type
+            { json: @delivery_type }
+    }
 
--- app:get "get_categories", "/get-category", ->
---   {json: category:select()}
+    "/products": respond_to {
+        GET: => 
+            @product = Product\select!
+            @write status: 404, "Not Found" unless @product
+            { json: @product }
+        POST: json_params =>
+            @product = Product\create
+                name: @params.name 
+                category: tonumber(@params.category) 
+                price: tonumber(@params.price) 
+                created_date: os.date("%Y-%m-%d") 
+                description: @params.description 
+                available: not not @params.available
+            { json: @product }
+    }
 
--- app:get "get_category", "/get-category/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   category_info = category:find {id}
---   return {json: {error: "Category not found"}, status: 404} unless category_info
---   {json: category_info}
+    "/product": respond_to {
+        PUT: json_params =>
+            @product = Product\find id: @params.id
+            @write status: 404, "Not Found" unless @product
+            @product\update @params
+            { json: @product }
+    }
 
--- -- PaymentType
+    "/product/:id": respond_to {
+        GET: => 
+            @product = Product\find id: @params.id
+            @write status: 404, "Not Found" unless @product
+            { json: @product }
+        DELETE: =>
+            @product = Product\find id: @params.id
+            @write status: 404, "Not Found" unless @product
+            @product\delete!
+            { json: "Item deleted" }
+    }
 
--- app:get "get_payment_types", "/get-payment-type", ->
---   {json: payment_type:select()}
+    "/purchases": respond_to {
+        GET: => 
+            @purchase = Purchase\select!
+            @write status: 404, "Not Found" unless @purchase
+            { json: @purchase }
+        POST: json_params =>
+            @purchase = Purchase\create 
+                id_product: tonumber(@params.id_product) 
+                id_user: tonumber(@params.id_user) 
+                price: tonumber(@params.price) 
+                quantity: tonumber(@params.quantity)
+                purchase_date: os.date("%Y-%m-%d")
+                delivery_type: tonumber(@params.delivery_type)
+                payment_type: tonumber(@params.payment_type)
+            { json: @purchase }
+    }
 
--- app:get "get_payment_type", "/get-payment-type/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   payment_type_info = payment_type:find id
---   return {json: {error: "Payment Type not found"}, status: 404} unless payment_type_info
---   {json: payment_type_info}
+    "/purchase": respond_to {
+        PUT: json_params =>
+            @purchase = Purchase\find id: @params.id
+            @write status: 404, "Not Found" unless @purchase
+            @purchase\update @params 
+            { json: @purchase }
+    }
 
--- -- DeliveryType
+    "/purchase/:id": respond_to {
+        GET: => 
+            @purchase = Purchase\find id: @params.id
+            @write status: 404, "Not Found" unless @purchase
+            { json: @purchase }
 
--- app:get "get_delivery_types", "/get-delivery-type", ->
---   {json: delivery_type:select()}
+        DELETE: =>
+            @purchase = Purchase\find id: @params.id
+            @write status: 404, "Not Found" unless @purchase
+            @purchase\delete!
+            { json: "Item deleted" } 
+    }
 
--- app:get "get_delivery_type", "/get-delivery-type/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   delivery_type_info = delivery_type:find id
---   return {json: {error: "Delivery Type not found"}, status: 404} unless delivery_type_info
---   {json: delivery_type_info}
+    "/users": respond_to {
+        GET: => 
+            @user = User\select!
+            @write status: 404, "Not Found" unless @user
+            { json: @user }
+        POST: json_params =>
+            @user = User\create 
+                name: @params.name 
+                surname: @params.surname
+                password: @params.password
+                address: @params.address
+                created_date: os.date("%Y-%m-%d") 
+            { json: @user }
+    }
 
--- -- Purchase
+    "/user": respond_to {
+        PUT: =>
+            @user = User\find id: @params.id
+            @write status: 404, "Not Found" unless @user
+            @user\update @params
+            { json: @user }
+    }
+    
+    "/user/:id": respond_to {
+        GET: => 
+            @user = User\find id: @params.id
+            @write status: 404, "Not Found" unless @user
+            { json: @user }
 
--- app:get "get_purchases", "/get-purchase", ->
---   purchases = purchase:select()
---   return {status: 404, json: "No purchases found."} unless purchases or #purchases > 0
---   {status: 200, json: purchases}
-
--- app:get "get_purchase", "/get-purchase/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   purchase_info = purchase:find {id}
---   return {json: {error: "Purchase not found"}, status: 404} unless purchase_info
---   {json: purchase_info}
-
--- app:post "add_purchase", "/add-purchase", ->
---   payload = @params
---   return {status: 404} unless payload
---   purchase:create {
---     id_product: tonumber(payload.id_product)
---     id_user: tonumber(payload.id_user)
---     price: tonumber(payload.price)
---     purchase_date: os.date("%Y-%m-%d")
---     quantity: tonumber(payload.quantity)
---     delivery_type: tonumber(payload.delivery_type)
---     payment_type: tonumber(payload.payment_type)
---   }
---   {status: 201}
-
--- app:put "update_purchase", "/update-purchase/:id", ->
---   payload = @params
---   return {status: 404} unless payload
---   purchase_info = purchase:find tonumber(payload.id)
---   return {status: 404, json: {message: "Purchase not found."}} unless purchase_info
---   update_purchase = {
---     id: tonumber(payload.id)
---     id_product: tonumber(payload.id_product)
---     id_user: tonumber(payload.id_user)
---     price: tonumber(payload.price)
---     purchase_date: os.date("%Y-%m-%d")
---     quantity: tonumber(payload.quantity)
---     delivery_type: tonumber(payload.delivery_type)
---     payment_type: tonumber(payload.payment_type)
---   }
---   product:update update_purchase
---   {status: 204, json: update_purchase}
-
--- app:delete "delete_purchase", "/delete-purchase/:id", ->
---   purchase_info = purchase:find tonumber(@params.id)
---   return {status: 404, json: {message: "Purchase not found"}} unless purchase_info
---   purchase_info:delete()
---   {status: 204}
-
--- -- Product
-
--- app:get "get_products", "/get-product", ->
---   products = product:select()
---   return {status: 404, json: "No product types found."} unless products or #products > 0
---   {status: 200, json: products}
-
--- app:get "get_product", "/get-product/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   product_info = product:find {id}
---   return {json: {error: "Product not found"}, status: 404} unless product_info
---   {json: product_info}
-
--- app:post "add_product", "/add-product", ->
---   payload = @params
---   return {status: 404} unless payload
---   product:create {
---     name: payload.name
---     category: tonumber(payload.category)
---     price: tonumber(payload.price)
---     created_date: os.date("%Y-%m-%d")
---     description: payload.description
---     available: not not payload.available
---   }
---   {status: 201}
-
--- app:put "update_product", "/update-product", ->
---   payload = @params
---   return {status: 404} unless payload
---   product_info = product:find tonumber(payload.id)
---   return {status: 404, json: {message: "Product not found."}} unless product_info
---   update_product = {
---     id: tonumber(payload.id)
---     name: payload.name
---     category: payload.category
---     price: tonumber(payload.price)
---     created_date: os.date("%Y-%m-%d")
---     description: payload.description
---     available: not not payload.available
---   }
---   product:update update_product
---   {status: 204, json: update_product}
-
--- app:delete "delete_product", "/delete-product/:id", ->
---   product_info = product:find tonumber(@params.id)
---   return {status: 404, json: {message: "Product not found"}} unless product_info
---   product_info:delete()
---   {status: 204}
-
--- -- User
-
--- app:get "get_users", "/get-user", ->
---   users = user:select()
---   return {status: 404, json: "No users found."} unless users or #users > 0
---   {status: 200, json: users}
-
--- app:get "get_user", "/get-user/:id", ->
---   id = tonumber @params.id
---   return {json: {error: "Invalid ID"}, status: 400} unless id
---   user_info = user:find {id}
---   return {json: {error: "User not found"}, status: 404} unless user_info
---   {json: user_info}
-
--- app:post "add_user", "/add-user", ->
---   payload = @params
---   return {status: 404} unless payload
---   user:create {
---     name: payload.name
---     surname: payload.surname
---     password: payload.password
---     address: payload.address
---     created_date: os.date("%Y-%m-%d")
---   }
---   {status: 201}
-
--- app:put "update_user", "/update-user/:id", ->
---   payload = @params
---   return {status: 404} unless payload
---   user_info = user:find tonumber(payload.id)
---   return {status: 404, json: {message: "Product not found."}} unless user_info
---   update_user = {
---     id: tonumber(payload.id)
---     name: payload.name
---     surname: payload.surname
---     password: payload.password
---     address: payload.address
---     created_date: os.date("%Y-%m-%d")
---   }
---   product:update update_user
---   {status: 204, json: update_user}
-
--- app:delete "delete_user", "/delete-user/:id", ->
---   user_info = user:find @params.id
---   return {status: 404, json: {message: "User not found"}} unless user_info
---   user_info:delete()
---   {status: 204}
-
--- app
+        DELETE: =>
+            @user = User\find id: @params.id
+            @write status: 404, "Not Found" unless @user
+            @user\delete!
+            { json: "Item deleted" } 
+    }
